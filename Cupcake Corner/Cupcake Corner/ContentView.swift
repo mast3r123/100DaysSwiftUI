29027509855428fe011a64bb083a5d51d22f93bd
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-03 14:15:56 -0400

    Initial screen with model and navigation link

--- Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -7,85 +7,42 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Paul Hudson"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
     
-    @State private var results = [Result]()
-    
+    @StateObject var order = Order()
     
     var body: some View {
-        
-//        VStack {
-            
-            AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-                if let image = phase.image {
-                    image.resizable().scaledToFit()
-                } else if phase.error != nil {
-                    Text("There was an error loading the image")
-                } else {
-                    ProgressView()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.qunatity)", value: $order.qunatity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special request?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery Details")
+                    }
                 }
             }
-            .frame(width: 200, height: 200)
-            
-            
-//            List(results, id: \.trackId) { item in
-//                VStack(alignment: .leading) {
-//                    Text(item.trackName)
-//                        .font(.headline)
-//                    Text(item.collectionName)
-//                }
-//            }
-//            .task {
-//                await loadData()
-//            }
-//        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=post+malone&entity=song") else {
-            print("Invalid URL")
-            return
+            .navigationTitle("Cupcake corner")
         }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-        
     }
 }
 
---
+++ Cupcake Corner/Cupcake Corner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by master on 6/3/22.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var qunatity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
