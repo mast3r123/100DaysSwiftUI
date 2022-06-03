30c17852396d8367734a62ed7085dfdf4b43aeda
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-03 16:55:33 -0400

    Checkout page, Address Page, Computed Properties for order

--- Cupcake Corner/Cupcake Corner/AddressView.swift
+++ Cupcake Corner/Cupcake Corner/AddressView.swift
@@ -12,7 +12,25 @@ struct AddressView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAdress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Checkout")
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
--- Cupcake Corner/Cupcake Corner/Order.swift
+++ Cupcake Corner/Cupcake Corner/Order.swift
@@ -23,4 +23,31 @@ class Order: ObservableObject {
     }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAdress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAdress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Double {
+        var cost = Double(qunatity) * 2
+        
+        cost += (Double(type) / 2)
+        
+        if extraFrosting {
+            cost += Double(qunatity)
+        }
+        
+        if addSprinkles {
+            cost += Double(qunatity) / 2
+        }
+        return cost
+    }
 }
