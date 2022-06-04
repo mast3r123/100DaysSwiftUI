109666ca825fb4a3ff45ff16f3ad1edd3e2e2514
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-04 12:06:21 -0400

    using urlsession to upload and fetch data

--- Cupcake Corner/Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/Cupcake Corner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -28,13 +31,43 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place Order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("Ok") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        
+        var request = URLRequest(url: url)
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.qunatity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on it's way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed")
+        }
     }
 }
 
