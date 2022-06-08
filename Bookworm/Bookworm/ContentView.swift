34b69f3bde2430adadb9c99b04f685058395a2b7
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-08 15:21:52 -0400

    Delete books

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -37,9 +37,17 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete { indexSet in
+                    deleteBooks(at: indexSet)
+                }
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -52,6 +60,15 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
