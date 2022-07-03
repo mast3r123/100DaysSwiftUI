8ac34d900be361a04724f05412fbdd70607bd769
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-02 21:23:55 -0400

    Wikipedia API, MVVM

---
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by master on 7/2/22.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        @Published private(set) var locations = [Location]()
+        
+        @Published var selectedPlace: Location?
+        
+        func addLocation() {
+            let location = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(location)
+        }
+        
+        func update(location: Location) {
+            
+            guard let selectedPlace = selectedPlace else {
+                return
+            }
+
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+        }
+    }
+
+}
--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,14 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-    @State private var locations = [Location]()
-    
-    @State private var selectedPlace: Location?
+    @StateObject private var viewModel = ViewModel()
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
                 MapAnnotation(coordinate: location.coordinate) {
                     VStack {
                         Image(systemName: "star.circle")
@@ -31,7 +28,7 @@ struct ContentView: View {
                             .fixedSize()
                     }
                     .onTapGesture {
-                        selectedPlace = location
+                        viewModel.selectedPlace = location
                     }
                 }
             }
@@ -49,8 +46,7 @@ struct ContentView: View {
                     Spacer()
                     
                     Button {
-                        let location = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
-                        locations.append(location)
+                        viewModel.addLocation()
                     } label: {
                         Image(systemName: "plus")
                     }
@@ -63,11 +59,9 @@ struct ContentView: View {
                 }
             }
         }
-        .sheet(item: $selectedPlace) { place in
+        .sheet(item: $viewModel.selectedPlace) { place in
             EditView(location: place) { location in
-                if let index = locations.firstIndex(of: place) {
-                    locations[index] = location
-                }
+                viewModel.update(location: location)
             }
         }
     }
