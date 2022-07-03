292bc9b6f4a5f42d6dd179c1cbba9689febca01a
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-02 21:32:42 -0400

    save and load data from documents directory

--- BucketList/BucketList/ContentView-ViewModel.swift
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -11,13 +11,34 @@ import MapKit
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
         
         @Published var selectedPlace: Location?
+        
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+            } catch {
+                
+            }
+        }
         
         func addLocation() {
             let location = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
             locations.append(location)
+            save()
         }
         
         func update(location: Location) {
@@ -29,6 +50,7 @@ extension ContentView {
             
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
             }
         }
     }
