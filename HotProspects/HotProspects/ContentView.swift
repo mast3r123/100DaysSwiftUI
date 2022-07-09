4a0919ff59d88c86bd394638ad0a9cb5e91cfbde
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-09 16:43:29 -0400

    Hot Prospects technique

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by master on 7/9/22.
+//
+
+import SwiftUI
+
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject private var user = User()
+    
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
