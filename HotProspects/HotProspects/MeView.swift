5fc0affd287fe27f7b9ac17181986a40fe631d7c
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-13 16:35:37 -0400

    QR code generator

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -5,11 +5,49 @@
 //  Created by master on 7/13/22.
 //
 
+import CoreImage.CIFilterBuiltins
+import CoreImage
 import SwiftUI
 
 struct MeView: View {
+    
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "sample@gmail.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                TextField("Email Address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name) \n \(emailAddress)"))
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+            }
+            .navigationTitle("Your Code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
