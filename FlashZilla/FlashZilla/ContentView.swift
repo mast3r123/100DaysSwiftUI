570fd34a16b4f460a74bf07282a330ef815e7003
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-21 14:48:40 -0400

    Non constructible change

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -6,69 +6,23 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct ContentView: View {
     
-    @State private var engine: CHHapticEngine?
+    @Environment(\.scenePhase) var scenePhase
     
     var body: some View {
         
         Text("Master")
-            .onAppear {
-                prepareHaptics()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
+                }
             }
-            .onTapGesture {
-                complexSuccess()
-            }
-        
-    }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("Error")
-        }
-    }
-    
-    func complexSuccess() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            
-            
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            
-            
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to start engine")
-        }
     }
 }
 
